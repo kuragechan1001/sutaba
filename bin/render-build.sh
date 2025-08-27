1d787048842bef3d21e0368d8f4fecf81ac5fcb8
kuragechan1001 <sct202410miyagi.airi.mizumoto@gmail.com>
2025-08-27 17:58:07 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
+# Rubyの依存関係をインストール
 bundle install
+
+# アセットのプリコンパイル（JSやCSSなど）
 bundle exec rake assets:precompile
+
+# DBマイグレーション（本番環境用）
 bundle exec rake db:migrate
