c5dee3a4869958eb5b1868dd29c7917ef75e434a
Mizumoto Airi <mizukawaairi@gmail.com>
2025-12-27 23:46:47 +0900

    Renderのエラー修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,8 @@ set -e
 bundle install
 
 # アセットのプリコンパイル（JSやCSSなど）
+# secret_key_baseが必要なため、ダミー値を設定（既に設定されている場合はそれを使用）
+export SECRET_KEY_BASE=${SECRET_KEY_BASE:-dummy_secret_key_base_for_asset_precompilation_only}
 bundle exec rake assets:precompile
 
 bundle exec rake assets:clean
